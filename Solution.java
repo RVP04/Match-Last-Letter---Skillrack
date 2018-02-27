338b467b1100a32a42b57cb88e3a6da2a2cd8493
VP <vidhyaprakash85@gmail.com>
2018-02-27 19:30:07 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -4,21 +4,28 @@ public class Hello {
     public static void main(String[] args) {
 	   
 	   Scanner sc = new Scanner(System.in);
+	   
+	    //getting two string as input
 	   String stringone = sc.next();
 	   String stringtwo = sc.next();
 	   
+	    //findout the length of the two strings.
 	   int stringOneLength = stringone.length();
 	   int stringTwoLength = stringtwo.length();
-	   boolean flag = true;
+	   
+	    //create a flag to match the character first and last character only once
+	    boolean flag = true;
 	   
 	   for(int i=0;i<stringOneLength;i++)
 	   {
+		//finding and matching the first and last character in two strings 
 	       if(stringone.charAt(i)==stringtwo.charAt(stringTwoLength-1)&&flag)
 	       {
-	           System.out.println(new StringBuilder(stringtwo).reverse().toString());
-	           flag = false;
+	           System.out.println(new StringBuilder(stringtwo).reverse().toString()); //reverse the string two
+	           flag = false; //once it is matched then this loop will not execute
 	       }
-	       else
+	       //if not match or already matched this block will be executed
+		else
 	       {
 	           System.out.println(stringone.charAt(i));
 	       }
